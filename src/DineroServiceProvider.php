b6e47c4fc8dae18501eb45ee79b5dddfdb9aa0b0
Jimmi <jimmi2908@gmail.com>
2023-06-26 18:31:24 +0200

    WIP

--- src/DineroServiceProvider.php
+++ src/DineroServiceProvider.php
@@ -2,24 +2,58 @@
 
 namespace OnCloud\Dinero;
 
-use OnCloud\Dinero\Commands\DineroCommand;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
 use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use OnCloud\Dinero\Commands\DineroCommand;
 
-class DineroServiceProvider extends PackageServiceProvider
+class DineroServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    public function register()
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('dinero')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_dinero_table')
-            ->hasCommand(DineroCommand::class);
+        $this->mergeConfigFrom(__DIR__.'/../config/dinero.php', 'dinero');
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->configurePublishing();
+        $this->configureRoutes();
+
+    }
+
+    /**
+     * Configure the publishable resources offered by the package.
+     *
+     * @return void
+     */
+    protected function configurePublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../stubs/dinero.php' => config_path('dinero.php'),
+            ], 'dinero-config');
+
+        }
+    }
+
+    /**
+     * Configure the routes offered by the application.
+     *
+     * @return void
+     */
+    protected function configureRoutes()
+    {
+        Route::group([
+            'namespace' => 'OnCloud\Dinero\Http\Controllers',
+            'domain' => config('dinero.domain', null),
+            'prefix' => config('dinero.prefix'),
+        ], function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
+        });
     }
 }
