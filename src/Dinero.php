7f36d2968e31f482bd36dc37549222e206a8f20e
FoksVHox <FoksVHox@users.noreply.github.com>
2023-06-30 21:21:24 +0000

    Fix styling

--- src/Dinero.php
+++ src/Dinero.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\Http;
 class Dinero
 {
     public static $baseUrl = 'https://api.dinero.dk/v1/';
+
     public function client()
     {
         return Http::withToken('')
-            ->baseUrl(self::$baseUrl. config('dinero.organization_id'));
+            ->baseUrl(self::$baseUrl.config('dinero.organization_id'));
     }
 }
