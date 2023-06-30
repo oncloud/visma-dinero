854b00203ad83a87164d8d8e3509d4eb2b70ff03
Jimmi <jimmi2908@gmail.com>
2023-06-30 23:22:07 +0200

    WIP

--- config/dinero.php
+++ config/dinero.php
@@ -4,4 +4,5 @@
 return [
     'client_id' => env('DINERO_CLIENT_ID'),
     'client_secret' => env('DINERO_CLIENT_SECRET'),
+    'organization_id' => env('DINERO_ORGANIZATION_ID'),
 ];
--- src/Dinero.php
+++ src/Dinero.php
@@ -2,6 +2,14 @@
 
 namespace OnCloud\Dinero;
 
+use Illuminate\Support\Facades\Http;
+
 class Dinero
 {
+    public static $baseUrl = 'https://api.dinero.dk/v1/';
+    public function client()
+    {
+        return Http::withToken('')
+            ->baseUrl(self::$baseUrl. config('dinero.organization_id'));
+    }
 }
