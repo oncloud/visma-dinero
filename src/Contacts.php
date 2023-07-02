5518cc3700522ee8b8097ba5e5946917027a757f
FoksVHox <FoksVHox@users.noreply.github.com>
2023-07-02 21:07:03 +0000

    Fix styling

--- src/Contacts.php
+++ src/Contacts.php
@@ -6,7 +6,6 @@ use OnCloud\Dinero\Facades\Dinero;
 
 class Contacts
 {
-
     /**
      * Retrieves a list of contacts for the organization ordered by UpdatedAt.
      *
@@ -22,7 +21,6 @@ class Contacts
     /**
      * Retrieves contact information for the contact with the given id.
      *
-     * @param string $guid
      * @return mixed
      */
     public function get(string $guid)
@@ -34,35 +32,13 @@ class Contacts
 
     /**
      * Add a new contact to the organization.
-     *
-     * @param string $name
-     * @param string $countryKey
-     * @param bool $isPerson
-     * @param bool $isMember
-     * @param bool $useCvr
-     * @param string|null $street
-     * @param string|null $zipCode
-     * @param string|null $city
-     * @param string|null $phone
-     * @param string|null $email
-     * @param string|null $webpage
-     * @param string|null $attPerson
-     * @param string|null $externalReference
-     * @param string|null $paymentConditionType
-     * @param int|null $paymentConditionNumberOfDays
-     * @param string|null $vatNumber
-     * @param string|null $eanNumber
-     * @param string|null $memberNumber
-     * @param string|null $companyTypeKey
-     * @param string|null $contactGuid
-     * @return mixed
      */
     public function create(string $name, string $countryKey, bool $isPerson, bool $isMember, bool $useCvr,
-                           string $street = null, string $zipCode = null, string $city = null, string $phone = null,
-                           string $email = null, string $webpage = null, string $attPerson = null,
-                           string $externalReference = null, string $paymentConditionType = null,
-                           int $paymentConditionNumberOfDays = null, string $vatNumber = null, string $eanNumber = null,
-                           string $memberNumber = null, string $companyTypeKey = null, string $contactGuid = null): mixed
+        string $street = null, string $zipCode = null, string $city = null, string $phone = null,
+        string $email = null, string $webpage = null, string $attPerson = null,
+        string $externalReference = null, string $paymentConditionType = null,
+        int $paymentConditionNumberOfDays = null, string $vatNumber = null, string $eanNumber = null,
+        string $memberNumber = null, string $companyTypeKey = null, string $contactGuid = null): mixed
     {
         return Dinero::client()
             ->post('/contacts', [
@@ -92,35 +68,13 @@ class Contacts
 
     /**
      * Updates an existing contact.
-     *
-     * @param string $guid
-     * @param string $name
-     * @param string $countryKey
-     * @param bool $isPerson
-     * @param bool $isMember
-     * @param bool $useCvr
-     * @param string|null $street
-     * @param string|null $zipCode
-     * @param string|null $city
-     * @param string|null $phone
-     * @param string|null $email
-     * @param string|null $webpage
-     * @param string|null $attPerson
-     * @param string|null $externalReference
-     * @param string|null $paymentConditionType
-     * @param int|null $paymentConditionNumberOfDays
-     * @param string|null $vatNumber
-     * @param string|null $eanNumber
-     * @param string|null $memberNumber
-     * @param string|null $companyTypeKey
-     * @return mixed
      */
     public function update(string $guid, string $name, string $countryKey, bool $isPerson, bool $isMember, bool $useCvr,
-                           string $street = null, string $zipCode = null, string $city = null, string $phone = null,
-                           string $email = null, string $webpage = null, string $attPerson = null,
-                           string $externalReference = null, string $paymentConditionType = null,
-                           int $paymentConditionNumberOfDays = null, string $vatNumber = null, string $eanNumber = null,
-                           string $memberNumber = null, string $companyTypeKey = null): mixed
+        string $street = null, string $zipCode = null, string $city = null, string $phone = null,
+        string $email = null, string $webpage = null, string $attPerson = null,
+        string $externalReference = null, string $paymentConditionType = null,
+        int $paymentConditionNumberOfDays = null, string $vatNumber = null, string $eanNumber = null,
+        string $memberNumber = null, string $companyTypeKey = null): mixed
     {
         return Dinero::client()
             ->put('/contacts/'.$guid, [
@@ -150,7 +104,6 @@ class Contacts
     /**
      * Deletes the contact with the given id.
      *
-     * @param string $guid
      * @return mixed
      */
     public function delete(string $guid)
@@ -163,7 +116,6 @@ class Contacts
     /**
      * Restores the contact with the given id.
      *
-     * @param string $guid
      * @return mixed
      */
     public function restore(string $guid)
