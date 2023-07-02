31106b9d48a6ee839c3c2074b3a51df38af68158
Jimmi <jimmi2908@gmail.com>
2023-07-02 23:22:08 +0200

    Implement Contact Notes api

--- src/Contacts.php
+++ src/Contacts.php
@@ -124,4 +124,80 @@ class Contacts
             ->post('/contacts/'.$guid.'/restore')
             ->json();
     }
+
+    /**
+     * Retrieves the notes for the contact with the given id.
+     *
+     * @param string $guid
+     * @return mixed
+     */
+    public function listContactNotes(string $guid)
+    {
+        return Dinero::client()
+            ->get('/contacts/'.$guid.'/notes')
+            ->json();
+    }
+
+    /**
+     * Retrieves the notes for the contact with the given id.
+     *
+     * @param string $guid
+     * @param string $noteGuid
+     * @return mixed
+     */
+    public function getContactNote(string $guid, string $noteGuid)
+    {
+        return Dinero::client()
+            ->get('/contacts/'.$guid.'/notes/'.$noteGuid)
+            ->json();
+    }
+
+/**
+     * Creates a note for the contact with the given id.
+     *
+     * @param string $guid
+     * @param string $text
+     * @return mixed
+     */
+    public function createContactNote(string $guid, string $text)
+    {
+        return Dinero::client()
+            ->post('/contacts/'.$guid.'/notes', [
+                'text' => $text,
+            ])
+            ->json();
+    }
+
+    /**
+     * Updates a note for the contact with the given id.
+     *
+     * @param string $guid
+     * @param string $noteGuid
+     * @param string $text
+     * @return mixed
+     */
+    public function updateContactNote(string $guid, string $noteGuid, string $text)
+    {
+        return Dinero::client()
+            ->put('/contacts/'.$guid.'/notes/'.$noteGuid, [
+                'text' => $text,
+            ])
+            ->json();
+    }
+
+    /**
+     * Deletes a note for the contact with the given id.
+     *
+     * @param string $guid
+     * @param string $noteGuid
+     * @return mixed
+     */
+    public function deleteContactNote(string $guid, string $noteGuid)
+    {
+        return Dinero::client()
+            ->delete('/contacts/'.$guid.'/notes/'.$noteGuid)
+            ->json();
+    }
+
+
 }
