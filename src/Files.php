68206a6a225e83b1ac5c0fc0162adaa558157b1a
Jimmi <jimmi2908@gmail.com>
2023-07-03 16:33:38 +0200

    Implement File API; Misses the upload operation.

--- src/Files.php
+++ src/Files.php
@@ -2,6 +2,47 @@
 
 namespace OnCloud\Dinero;
 
+use OnCloud\Dinero\Facades\Dinero;
+
 class Files
 {
+    /**
+     * Download the specified file. Returns the file with a certain file guid.
+     *
+     * @param string $fileGuid
+     * @return mixed
+     */
+    public function get(string $fileGuid)
+    {
+        return Dinero::client()
+            ->get('/files/'.$fileGuid)
+            ->json();
+    }
+
+    /**
+     * List files. Lists all files in file archive.
+     *
+     * @param array $extensions
+     * @param string|null $uploadedBefore
+     * @param string|null $uploadedAfter
+     * @param int|null $fileStatus
+     * @param int|null $page
+     * @param int|null $pageSize
+     * @return mixed
+     */
+    public function list(array $extensions = [], string $uploadedBefore = null, string $uploadedAfter = null, int $fileStatus = null, int $page = null, int $pageSize = null)
+    {
+        return Dinero::client()
+            ->withQueryParameters([
+                'extensions' => $extensions,
+                'uploadedBefore' => $uploadedBefore,
+                'uploadedAfter' => $uploadedAfter,
+                'fileStatus' => $fileStatus,
+                'page' => $page,
+                'pageSize' => $pageSize,
+            ])
+            ->get('/files')
+            ->json();
+    }
+
 }
