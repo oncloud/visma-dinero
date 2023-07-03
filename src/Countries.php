c8cb0318220eb28e8470183b42a302706828c939
Jimmi <jimmi2908@gmail.com>
2023-07-03 16:13:49 +0200

    Implement Country API

--- src/Countries.php
+++ src/Countries.php
@@ -2,6 +2,17 @@
 
 namespace OnCloud\Dinero;
 
+use OnCloud\Dinero\Facades\Dinero;
+
 class Countries
 {
+    /**
+     * List countries. Get the list of countries.
+     */
+    public function get()
+    {
+        return Dinero::client()
+            ->get('/countries')
+            ->json();
+    }
 }
