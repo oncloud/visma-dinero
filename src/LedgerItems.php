d8bf241f8545bbdfd04d05071b2362aa84801235
FoksVHox <FoksVHox@users.noreply.github.com>
2023-07-18 14:12:17 +0000

    Fix styling

--- src/LedgerItems.php
+++ src/LedgerItems.php
@@ -7,8 +7,6 @@ class LedgerItems
     /**
      * Get ledger items.
      *
-     * @param string $id
-     * @param string|null $version
      * @return mixed
      */
     public function get(string $id, string $version = null)
@@ -16,7 +14,7 @@ class LedgerItems
         return Dinero::client()
             ->post('/ledgeritems/ledgers/', [
                 'id' => $id,
-                'version' => $version
+                'version' => $version,
             ])
             ->json();
     }
@@ -24,12 +22,6 @@ class LedgerItems
     /**
      * Add ledger item. Add a new ledger item to the ledger. The maximum number of ledger items in the ledger is 1000.
      *
-     * @param string|null $id
-     * @param string|null $version
-     * @param int|null $voucherNumber
-     * @param string|null $voucherDate
-     * @param string|null $fileGuid
-     * @param array $lines
      * @return mixed
      */
     public function add(string $id = null, string $version = null, int $voucherNumber = null, string $voucherDate = null, string $fileGuid = null, array $lines = [])
@@ -41,7 +33,7 @@ class LedgerItems
                 'voucherNumber' => $voucherNumber,
                 'voucherDate' => $voucherDate,
                 'fileGuid' => $fileGuid,
-                'lines' => $lines
+                'lines' => $lines,
             ])
             ->json();
     }
@@ -49,12 +41,6 @@ class LedgerItems
     /**
      * Update ledger items. Update a ledger item to the ledger. It is required to send version and id for each ledger item.
      *
-     * @param string|null $id
-     * @param string|null $version
-     * @param int|null $voucherNumber
-     * @param string|null $voucherDate
-     * @param string|null $fileGuid
-     * @param array $lines
      * @return mixed
      */
     public function update(string $id = null, string $version = null, int $voucherNumber = null, string $voucherDate = null, string $fileGuid = null, array $lines = [])
@@ -66,7 +52,7 @@ class LedgerItems
                 'voucherNumber' => $voucherNumber,
                 'voucherDate' => $voucherDate,
                 'fileGuid' => $fileGuid,
-                'lines' => $lines
+                'lines' => $lines,
             ])
             ->json();
     }
@@ -75,8 +61,6 @@ class LedgerItems
      * Send ledger itmes to booking. Starting the asynchronous booking process. The ongoing status can be fetch from the
      * status endpoint.
      *
-     * @param string $id
-     * @param string|null $version
      * @return array|mixed
      */
     public function book(string $id, string $version = null)
@@ -84,7 +68,7 @@ class LedgerItems
         return Dinero::client()
             ->post('/ledgeritems/book/', [
                 'id' => $id,
-                'version' => $version
+                'version' => $version,
             ])
             ->json();
     }
@@ -92,14 +76,13 @@ class LedgerItems
     /**
      * Get ledger item status. Get status for ledger items sent to booking.
      *
-     * @param string $id
      * @return array|mixed
      */
     public function status(string $id)
     {
         return Dinero::client()
             ->post('/ledgeritems/status/', [
-                'id' => $id
+                'id' => $id,
             ])
             ->json();
     }
@@ -107,8 +90,6 @@ class LedgerItems
     /**
      * Delete multiple ledger items. Deletes a list of ledger items.
      *
-     * @param string $id
-     * @param string|null $version
      * @return array|mixed
      */
     public function delete(string $id, string $version = null)
@@ -116,7 +97,7 @@ class LedgerItems
         return Dinero::client()
             ->delete('/ledgeritems/delete', [
                 'id' => $id,
-                'version' => $version
+                'version' => $version,
             ])
             ->json();
     }
