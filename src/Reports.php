affd89887f83c03c3cb20b35f39f554759efa47d
Jimmi <jimmi2908@gmail.com>
2023-07-24 22:13:07 +0200

    Implement Reports API

--- src/Reports.php
+++ src/Reports.php
@@ -4,4 +4,105 @@ namespace OnCloud\Dinero;
 
 class Reports
 {
+    /**
+     * Get saldo balance report
+     *
+     * @param string $accountYear
+     * @param string|null $showZeroAccount
+     * @param string|null $showAccountNo
+     * @param string|null $includeSummeryAccount
+     * @param string|null $includeLedgerEntries
+     * @param string|null $showVatType
+     * @return mixed
+     */
+    public function getSaldoBalanceReport(string $accountYear, string $showZeroAccount = null, string $showAccountNo = null,
+        string $includeSummeryAccount = null, string $includeLedgerEntries = null, string $showVatType = null)
+    {
+        return Dinero::client()
+            ->withQueryParameters([
+                'showZeroAccount' => $showZeroAccount,
+                'showAccountNo' => $showAccountNo,
+                'includeSummeryAccount' => $includeSummeryAccount,
+                'includeLedgerEntries' => $includeLedgerEntries,
+                'showVatType' => $showVatType,
+            ])
+            ->get($accountYear.'/reports/saldo')
+            ->json();
+    }
+
+    /**
+     * Get result report
+     *
+     * @param string $accountYear
+     * @param string|null $showZeroAccount
+     * @param string|null $showAccountNo
+     * @param string|null $includeSummeryAccount
+     * @param string|null $includeLedgerEntries
+     * @param string|null $showVatType
+     * @return mixed
+     */
+    public function getResultReport(string $accountYear, string $showZeroAccount = null, string $showAccountNo = null,
+        string $includeSummeryAccount = null, string $includeLedgerEntries = null, string $showVatType = null)
+    {
+        return Dinero::client()
+            ->withQueryParameters([
+                'showZeroAccount' => $showZeroAccount,
+                'showAccountNo' => $showAccountNo,
+                'includeSummeryAccount' => $includeSummeryAccount,
+                'includeLedgerEntries' => $includeLedgerEntries,
+                'showVatType' => $showVatType,
+            ])
+            ->get($accountYear.'/reports/result')
+            ->json();
+    }
+
+    /**
+     * Get Primo balance report
+     *
+     * @param string $accountYear
+     * @param string|null $showZeroAccount
+     * @param string|null $showAccountNo
+     * @param string|null $includeSummeryAccount
+     * @param string|null $showVatType
+     * @return mixed
+     */
+    public function getPrimoBalanceReport(string $accountYear, string $showZeroAccount = null, string $showAccountNo = null,
+                                          string $includeSummeryAccount = null, string $showVatType = null)
+    {
+        return Dinero::client()
+            ->withQueryParameters([
+                'showZeroAccount' => $showZeroAccount,
+                'showAccountNo' => $showAccountNo,
+                'includeSummeryAccount' => $includeSummeryAccount,
+                'showVatType' => $showVatType,
+            ])
+            ->get($accountYear.'/reports/primo')
+            ->json();
+    }
+
+    /**
+     * Get balance report
+     *
+     * @param string $accountYear
+     * @param string|null $showZeroAccount
+     * @param string|null $showAccountNo
+     * @param string|null $includeSummeryAccount
+     * @param string|null $includeLedgerEntries
+     * @param string|null $showVatType
+     * @return mixed
+     */
+    public function getBalanceReport(string $accountYear, string $showZeroAccount = null, string $showAccountNo = null,
+                                     string $includeSummeryAccount = null, string $includeLedgerEntries = null, string $showVatType = null)
+    {
+        return Dinero::client()
+            ->withQueryParameters([
+                'showZeroAccount' => $showZeroAccount,
+                'showAccountNo' => $showAccountNo,
+                'includeSummeryAccount' => $includeSummeryAccount,
+                'includeLedgerEntries' => $includeLedgerEntries,
+                'showVatType' => $showVatType,
+            ])
+            ->get($accountYear.'/reports/balance')
+            ->json();
+    }
 }
