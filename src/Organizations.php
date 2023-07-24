0300107dbda6f944b7e9a554f43d21548b9bcc49
Jimmi <jimmi2908@gmail.com>
2023-07-24 16:30:00 +0200

    Implement Organizations API

--- src/Organizations.php
+++ src/Organizations.php
@@ -4,4 +4,15 @@ namespace OnCloud\Dinero;
 
 class Organizations
 {
+    /**
+     * List organization. Gets a list of the users' organizations.
+     *
+     * @return mixed
+     */
+    public function list()
+    {
+        return Dinero::client()
+            ->get('/organizations')
+            ->json();
+    }
 }
