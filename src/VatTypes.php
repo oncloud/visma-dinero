1811be965ac582e1cbc02fa27bb26880aaae25c1
Jimmi <jimmi2908@gmail.com>
2023-08-04 15:20:01 +0200

    Implement VatTypes API

--- src/VatTypes.php
+++ src/VatTypes.php
@@ -4,4 +4,15 @@ namespace OnCloud\Dinero;
 
 class VatTypes
 {
+    /**
+     * List vat types. Gets a list of all of the organization's VAT types.
+     *
+     * @return mixed
+     */
+    public function list()
+    {
+        return Dinero::client()
+            ->get('/vatTypes')
+            ->json();
+    }
 }
