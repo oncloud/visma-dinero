ad08fcc7524f8dd3de20cff611be29436dff0346
Jimmi <jimmi2908@gmail.com>
2023-08-04 14:22:30 +0200

    Implement SMS API

--- src/Sms.php
+++ src/Sms.php
@@ -2,6 +2,34 @@
 
 namespace OnCloud\Dinero;
 
+use OnCloud\Dinero\Facades\Dinero;
+
 class Sms
 {
+    /**
+     * Send sms with link to invoice. SMS has special terms and usage will be invoiced. By using the Sms API you accept
+     * the terms which can be found here: https://dinero.dk/sikkerhed/retningslinjer-og-vilkaar-for-api-partnere/
+     *
+     * @param string $voucherGuid
+     * @param string $receiverPhoneNumber
+     * @param string $receiverName
+     * @param string $timestamp
+     * @param string $isReminder
+     * @param string $message
+     * @return mixed
+     */
+    public function send($voucherGuid, $receiverPhoneNumber, $receiverName, $timestamp, $isReminder, $message)
+    {
+        return Dinero::client()
+            ->post('/sms'.$voucherGuid.'/send-sms', [
+                'voucherGuid' => $voucherGuid,
+                'receiverPhoneNumber' => $receiverPhoneNumber,
+                'receiverName' => $receiverName,
+                'timestamp' => $timestamp,
+                'isReminder' => $isReminder,
+                'message' => $message,
+            ])
+            ->json();
+    }
+
 }
